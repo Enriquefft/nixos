cded187d5891efb9296c1062bc625e0f303bf648
Enriquefft <enriquefft2001@gmail.com>
2026-02-28 17:10:29 -0500

    chore: disable zsh completion warning for root

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -10,4 +10,14 @@
     # https://github.com/nix-community/home-manager/issues/108
     enableCompletion = false;
   };
+
+  # Suppress "insecure directories" warning when root uses zsh
+  # (completion dirs are owned by normal user via home-manager)
+  environment.variables.ZSH_DISABLE_COMPFIX = "true";
+
+  # System-wide zsh init so all users (including root) get zoxide + cd alias
+  programs.zsh.interactiveShellInit = ''
+    eval "$(zoxide init zsh)"
+    alias cd=z
+  '';
 }
