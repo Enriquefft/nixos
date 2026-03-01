1b17d3e5dd0de695803fb6409729a149fe583751
Enriquefft <enriquefft2001@gmail.com>
2026-03-01 15:22:31 -0500

    chore: update sudo whitelist

--- security.nix
+++ security.nix
@@ -17,11 +17,34 @@ in {
       extraConfig = ''
         Defaults pwfeedback
       '';
+      execWheelOnly = true;
     };
 
     doas = {
       enable = true;
       extraRules = [
+        # Passwordless commands (whitelist for autonomous execution)
+        {
+          users = [ constants.user.name ];
+          cmd = "/run/current-system/sw/bin/nixos-rebuild";
+          noPass = true;
+        }
+        {
+          users = [ constants.user.name ];
+          cmd = "/run/current-system/sw/bin/systemctl";
+          noPass = true;
+        }
+        {
+          users = [ constants.user.name ];
+          cmd = "/run/current-system/sw/bin/nix-collect-garbage";
+          noPass = true;
+        }
+        {
+          users = [ constants.user.name ];
+          cmd = "/run/current-system/sw/bin/journalctl";
+          noPass = true;
+        }
+        # Everything else with password persist
         {
           users = [ constants.user.name ];
           keepEnv = true;
@@ -30,4 +53,17 @@ in {
       ];
     };
   };
+
+  # Passwordless commands for sudo (whitelist for autonomous execution)
+  security.sudo.extraRules = [
+    {
+      users = [ constants.user.name ];
+      commands = [
+        { command = "/run/current-system/sw/bin/nixos-rebuild"; options = [ "NOPASSWD" ]; }
+        { command = "/run/current-system/sw/bin/systemctl"; options = [ "NOPASSWD" ]; }
+        { command = "/run/current-system/sw/bin/nix-collect-garbage"; options = [ "NOPASSWD" ]; }
+        { command = "/run/current-system/sw/bin/journalctl"; options = [ "NOPASSWD" ]; }
+      ];
+    }
+  ];
 }
