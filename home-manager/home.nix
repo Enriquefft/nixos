c3322abec61595c3572c14c3213e905946445b44
Enriquefft <enriquefft2001@gmail.com>
2026-03-11 04:32:37 -0500

    chore(home-manager): set Brave as default browser
    - Update MIME associations from Firefox to Brave
    - Default handler for http, https, html, pdf, figma

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -40,12 +40,12 @@
     mime.enable = true;
     mimeApps = {
       defaultApplications = {
-        "application/pdf" = "firefox.desktop";
-        "text/html" = "firefox.desktop";
-        "x-scheme-handler/http" = "firefox.desktop";
-        "x-scheme-handler/https" = "firefox.desktop";
-        "x-scheme-handler/about" = "firefox.desktop";
-        "x-scheme-handler/unknown" = "firefox.desktop";
+        "application/pdf" = "brave-browser.desktop";
+        "text/html" = "brave-browser.desktop";
+        "x-scheme-handler/http" = "brave-browser.desktop";
+        "x-scheme-handler/https" = "brave-browser.desktop";
+        "x-scheme-handler/about" = "brave-browser.desktop";
+        "x-scheme-handler/unknown" = "brave-browser.desktop";
         "x-scheme-handler/figma" = "figma-linux.desktop";
 
       };
